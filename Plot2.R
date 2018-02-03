b049ccd28b5bc2532ffff30915d9c0f5e53ab3d0
kolkokolko <danail.hristov@kpn.com>
2018-02-03 11:43:46 +0100

    final

--- Plot2.R
+++ Plot2.R
@@ -1,12 +1,21 @@
+# add libraries
 library(lubridate)
 library(dplyr)
+
+#set working dir
 setwd("C:/Users/hrist499/AppData/Local/lxss/home/dankata/Coursera/DataScience/ExploratoryDataAnalysis/")
+
+#read file 
 rawData <-read.csv("../household_power_consumption.txt",sep=";",na.strings = "?")
+
+
+#Subset the required dates and convert the date time to posix
 rd <- tbl_df(subset(rawData, Date %in% c("1/2/2007","2/2/2007")))
 rd <- mutate(rd,dateTime = paste(Date,Time))
 rd$dateTime <- dmy_hms(rd$dateTime)
 str(rd)
 
+#Create Plot
 png("Plot_2.png",height = 480,width = 480)
 with(rd, plot(dateTime,Global_active_power,pch=20,type="l",ylab="Global Active Power (Kilowatts)"))
 dev.off()
